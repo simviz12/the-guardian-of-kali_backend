8012163a394f62014b653301983601cd54797005
simviz12 <carlos.benavidesbu@gmail.com>
2026-09-10 16:20:12 -0500

    feat: add Session entity (#6)

--- src/domain/entities/__init__.py
+++ src/domain/entities/__init__.py
@@ -1,5 +1,6 @@
 """Domain entities package initialization."""
 from src.domain.entities.command import Command
 from src.domain.entities.target import Target
+from src.domain.entities.session import Session
 
-__all__ = ["Command", "Target"]
+__all__ = ["Command", "Target", "Session"]
